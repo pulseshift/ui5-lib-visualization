70c0793dab4d9bf78672c90368293bc29e32c371
Unknown <lena.srd@hotmail.de>
2017-12-19 15:08:04 +0100

    c3-extended commit again (did not work?)

--- src/vendor/c3-extended.js
+++ src/vendor/c3-extended.js
@@ -3486,7 +3486,12 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         area = config.axis_rotated ? area.x0(value0).x1(value1).y(xValue) : area.x(xValue).y0(config.area_above ? 0 : value0).y1(value1);
         if (!config.line_connectNull) {
             area = area.defined(function (d) {
-                return d.ribbonYs !== null; //FIXME: check difference value/ribbonYs
+              if($$.isRibbonType(d)){
+                return d.ribbonYs !== null;
+              }
+                else{
+                return d.value !== null;
+                }
             });
         }
 
@@ -3506,18 +3511,19 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
                 // ===== START OPAL EXTENSION =====
                 // in case of the ribbon type, the null defined sequence in the beginning needs to be cut off
                 if($$.isRibbonType(d)) {
-                    var sliceStart = 0, valuesLength = values.length;
+                    var sliceStart = 0, valuesLength = values.length, sliceEnd = 0;
                     for(var i=0; i<valuesLength; i++){
                         if (values[i].ribbonYs.low === null && values[i].ribbonYs.high === null)
                             sliceStart++;
                         else break;
                     }
-                    // var sliceStart = values.reduce(function (currentSliceStart, val) {
-                    //     return val.ribbonYs.low === null && val.ribbonYs.high === null && currentSliceStart === 0
-                    //         ? currentSliceStart++
-                    //         : currentSliceStart;
-                    // }, 0);
-                    values = values.slice(sliceStart, (values.length));
+
+                    for(var i=valuesLength-1; i>0; i--){
+                      if (values[i].ribbonYs.low === null && values[i].ribbonYs.high === null)
+                          sliceEnd++;
+                      else break;
+                  }
+                    values = values.slice(sliceStart, (values.length-sliceEnd));
                 }
                 // ===== END OPAL EXTENSION =====
 
@@ -4150,7 +4156,8 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         var $$ = this,
             config = $$.config,
             dataToShow = selectedData.filter(function (d) {
-            return d && isValue(d.value);
+                if (d) {var ribbonIsValue = (d.ribbonYs == undefined) ? null : (isValue(d.ribbonYs.high) && isValue(d.ribbonYs.low));}
+                return d && (isValue(d.value) || ribbonIsValue);
         }),
             focusEl = $$.main.selectAll('line.' + CLASS.xgridFocus),
             xx = $$.xx.bind($$);
@@ -4251,34 +4258,62 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         }
     };
     c3_chart_internal_fn.getTooltipContent = function (d, defaultTitleFormat, defaultValueFormat, color) {
-        var $$ = this,
-            config = $$.config,
+        var $$ = this, config = $$.config,
             titleFormat = config.tooltip_format_title || defaultTitleFormat,
-            nameFormat = config.tooltip_format_name || function (name) {
-            return name;
-        },
+            nameFormat = config.tooltip_format_name || function (name) { return name; },
             valueFormat = config.tooltip_format_value || defaultValueFormat,
-            text,
-            i,
-            title,
-            value,
-            name,
-            bgcolor,
+            text, i, title, value, name, bgcolor,
             orderAsc = $$.isOrderAsc();
 
+            //create an array that contains any CI's high AND low values as seperate entries
+            var completeArray = [];//.concat(d);
+            var offset=0;
+
+            for (i = 0; i < d.length; i++) {
+                if (! (d[i])) {
+                    offset++;
+                    continue; }
+                
+                //append the data for one graph
+                completeArray.push({
+                    id: d[i].id,
+                    value: d[i].value,
+                    name: d[i].name,
+                    index: d[i].index,
+                    x: d[i].x
+                });
+                
+                
+                //append high and low data for ribbon type
+                if($$.isRibbonType(d[i])){
+                    var sName = d[i].name;
+                    //create a new element and append it to the array for the low value
+                    completeArray.push({
+                        id: d[i].id,
+                        value: d[i].ribbonYs.low,
+                        name: sName.concat(" low"),
+                        index: d[i].index,
+                        ribbonYs: d[i].ribbonYs,
+                        x: d[i].x
+                    });
+                    //change original CI element to represent the high value
+                    completeArray[i-offset].value = d[i].ribbonYs.high;
+                    completeArray[i-offset].name = sName.concat(" high");
+                }
+            }
+
+
         if (config.data_groups.length === 0) {
-            d.sort(function (a, b) {
-                var v1 = a ? a.value : null,
-                    v2 = b ? b.value : null;
+            completeArray.sort(function(a, b){
+                var v1 = a ? a.value : null, v2 = b ? b.value : null;
                 return orderAsc ? v1 - v2 : v2 - v1;
             });
         } else {
             var ids = $$.orderTargets($$.data.targets).map(function (i) {
                 return i.id;
             });
-            d.sort(function (a, b) {
-                var v1 = a ? a.value : null,
-                    v2 = b ? b.value : null;
+            completeArray.sort(function(a, b) {
+                var v1 = a ? a.value : null, v2 = b ? b.value : null;
                 if (v1 > 0 && v2 > 0) {
                     v1 = a ? ids.indexOf(a.id) : null;
                     v2 = b ? ids.indexOf(b.id) : null;
@@ -4287,26 +4322,22 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
             });
         }
 
-        for (i = 0; i < d.length; i++) {
-            if (!(d[i] && (d[i].value || d[i].value === 0))) {
-                continue;
-            }
+        for (i = 0; i < completeArray.length; i++) {
+            if (! (completeArray[i] && (completeArray[i].value || completeArray[i].value === 0))) { continue; }
 
-            if (!text) {
-                title = sanitise(titleFormat ? titleFormat(d[i].x) : d[i].x);
+            if (! text) {
+                title = sanitise(titleFormat ? titleFormat(completeArray[i].x) : completeArray[i].x);
                 text = "<table class='" + $$.CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
             }
 
-            value = sanitise(valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index, d));
+            value = sanitise(valueFormat(completeArray[i].value, completeArray[i].ratio, completeArray[i].id, completeArray[i].index, completeArray));
             if (value !== undefined) {
                 // Skip elements when their name is set to null
-                if (d[i].name === null) {
-                    continue;
-                }
-                name = sanitise(nameFormat(d[i].name, d[i].ratio, d[i].id, d[i].index));
-                bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+                if (completeArray[i].name === null) { continue; }
+                name = sanitise(nameFormat(completeArray[i].name, completeArray[i].ratio, completeArray[i].id, completeArray[i].index));
+                bgcolor = $$.levelColor ? $$.levelColor(completeArray[i].value) : color(completeArray[i].id);
 
-                text += "<tr class='" + $$.CLASS.tooltipName + "-" + $$.getTargetSelectorSuffix(d[i].id) + "'>";
+                text += "<tr class='" + $$.CLASS.tooltipName + "-" + $$.getTargetSelectorSuffix(completeArray[i].id) + "'>";
                 text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
                 text += "<td class='value'>" + value + "</td>";
                 text += "</tr>";
@@ -4358,7 +4389,8 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         var tWidth, tHeight, position;
         var forArc = $$.hasArcType(),
             dataToShow = selectedData.filter(function (d) {
-            return d && isValue(d.value);
+            if (d) {var ribbonIsValue = (d.ribbonYs == undefined) ? null : (isValue(d.ribbonYs.high) && isValue(d.ribbonYs.low));}
+            return d && (isValue(d.value) || ribbonIsValue);
         }),
             positionFunction = config.tooltip_position || c3_chart_internal_fn.tooltipPosition;
         if (dataToShow.length === 0 || !config.tooltip_show) {
